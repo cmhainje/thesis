95aa3b82801cec4b9766f761f40322b61993b5c7
Connor Hainje <cmhainje@gmail.com>
2021-02-23 20:56:36 -0500

    Remove old simulation suite

--- simulation/halos_only/sgr/local_run_gizmo.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-module purge
-module load openmpi/gcc/1.10.2/64 gsl/2.4 fftw/gcc/openmpi-1.10.2/3.3.4 hdf5/gcc/1.8.16
-cd /home/chainje
-mpirun ./gizmo-public/GIZMO ./thesis/simulation/halos_only/sgr/gizmo.param
